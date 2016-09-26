950fea7d45f79d6b179a7abff5cd6118954fc5e9
Flávio Micheletti <flaviomicheletti@hotmail.com>
2016-09-26 15:00:10 -0300

    frontend

--- frontend/artigos.js
+++ frontend/artigos.js
@@ -1,29 +1,51 @@
 var Artigo = function(){
 
     this.create = function(callback){
-        $.post("/fulia/backend/artigos/create.php", "artigo=" + JSON.stringify(this), function( data ) {
-          var resp = JSON.parse(data);
-          console.log("dentro da funcao create - ", resp);
-          callback();
+        var me = this;
+        $.post("/fulia/backend/artigos/create.php", "artigo=" + JSON.stringify(this), function (data) {
+            var resp = JSON.parse(data);
+            console.log("artigo.create(): ", resp);
+            me.id = resp.artigo_id;
+            callback(resp);
         })
-          .fail(function(request, status, error) {
+        .fail(function(request, status, error) {
             throw "artigo.create()=" + request.responseText;
-          });    
-      };
+        });    
+    };
+
+    this.read = function (id, callback) {
+        if (!id) throw "artigo.read(): " + "uhuhu  como ler o artigo sem o id ?";
+        $.post("/fulia/backend/artigos/read.php", "id=" + id, function (data) {
+            var resp = JSON.parse(data);
+            console.log("artigo.read(): ", resp);
+            callback(resp);
+        })
+        .fail(function (request, status, error) {
+            throw "artigo.read():" + request.responseText;
+        });
+    };
     
+    this.update = function (callback) {
+        if (!this.id) throw "artigo.update(): " + "como atualizar o artigo sem o id ?";
+        $.post("/fulia/backend/artigos/update.php", "artigo=" + JSON.stringify(this), function (data) {
+            var resp = JSON.parse(data);
+            console.log("artigo.update(): ", resp);
+            callback(resp);
+        })
+        .fail(function(request, status, error) {
+            throw "artigo.update()=" + request.responseText;
+        });        
+    };
+    
+    this.delete = function (id, callback) {
+        if (!id) throw "artigo.delete(): " + "uhuhu  como deletar o artigo sem o id ?";
+        $.post("/fulia/backend/artigos/delete.php", "id=" + id, function( data ) {
+            var resp = JSON.parse(data);
+            console.log("artigo.delete(): ", resp);
+            callback(resp);
+        })
+        .fail(function(request, status, error) {
+            throw "artigo.delete()=" + request.responseText;
+        });        
+    };
 };
-
-
-
-function artigo_read(callback, id) {
-    if (!id) throw "artigo.read():" + "uhuhu  como ler o artigo sem o id ?";
-
-    $.post("/fulia/backend/artigos/read.php", "id=" + id, function (data) {
-        var resp = JSON.parse(data);
-        console.log(resp, id);
-        callback(resp);
-    }).fail(function (request, status, error) {
-        throw "artigo.create():" + request.responseText;
-    });
-}
-
--- frontend/tests/artigos-test.js
+++ frontend/tests/artigos-test.js
@@ -1,22 +1,8 @@
 //
-// Post Crud
+// Artigo Crud
 //
-
 QUnit.module("artigo");
-
-var temp = {};
-
-//
-// Create
-//
 QUnit.test("crud", function (assert) {
-    var done = function () {
-        assert.async();
-    }
-  
-    //
-    // Create
-    //
     var artigo = new Artigo();
     artigo.url = 'foo/';
     artigo.titulo = 'Foo';
@@ -28,105 +14,18 @@ QUnit.test("crud", function (assert) {
     artigo.dt_atualizacao = '2013-04-10';
     artigo.dt_criacao = '2013-04-10';
     artigo.ordem = 9;
-    artigo.create(done);
+    artigo.create(function () {
+        artigo.read(artigo.id, function () {
+            artigo.titulo = "Foo die";
+            artigo.resumo = "Mais um foo";
+            artigo.nivel = 'basico';
+            artigo.update(function () {
+                artigo.delete(artigo.id, function () {
+                    // done
+                    assert.async();
+                });
+            });
+        });
+    });
     assert.ok(true);
-    //assert.async('artigo criado com sucesso!', resp.msg);
-  
-//  var artigo = {
-//    url: 'foo/',
-//    titulo: 'Foo',
-//    resumo: 'Apenas um foo',
-//    keywords: 'foodie',
-//    nivel: 'intermediario',
-//    secao: 'php',
-//    autor: 'euzinho',
-//    dt_atualizacao: '2013-04-10',
-//    dt_criacao: '2013-04-10',
-//    ordem: 9
-//  };
-//
-//  $.post("/fulia/backend/artigos/create.php", "artigo=" + JSON.stringify(artigo), function( data, temp ) {
-//    var resp = JSON.parse(data);
-//    temp.id =  resp.artigo_id;
-//    console.log(resp, temp);
-//    assert.equal('artigo criado com sucesso!', resp.msg);
-//    done();
-//  })
-//    .fail(function(request, status, error) {
-//      throw "artigo.create()=" + request.responseText;
-//    });
-});
-
-//
-// Read
-//
-QUnit.skip("read", function( assert, temp ) {
-
-  console.log(temp);
-
-  var done = function(resp) {
-    assert.async();
-    assert.equal('artigo carregado com sucesso!', resp.msg);
-  }
-  artigo_read(done, temp);
-
-//   if (!temp.id) throw "artigo.create():" + "como ler o artigo sem o id ?";
-//
-//   $.post("/fulia/backend/artigos/read.php", "id=" + temp.id, function( data ) {
-//     var resp = JSON.parse(data);
-//     console.log(resp, temp);
-//     assert.equal('artigo carregado com sucesso!', resp.msg);
-//     done();
-//   })
-//     .fail(function(request, status, error) {
-//       throw "artigo.create():" + request.responseText;
-//     });
-});
-
-//
-// Update
-//
-QUnit.skip("update", function( assert ) {
-  var done = assert.async();
-
-  if (!temp.id) throw "artigo.update():" + "como atualizar o artigo sem o id ?";
-
-  var artigo = {
-    id: temp.id,
-    url: 'new-foo/',
-    titulo: 'new Foo',
-    resumo: 'Um outro foo',
-    keywords: 'foolish',
-    nivel: 'intermediario',
-    secao: 'js',
-    autor: 'euzinho',
-    dt_atualizacao: '2013-04-10',
-    dt_criacao: '2013-04-10',
-    ordem: 9
-  };
-  $.post("/fulia/backend/artigos/update.php", "artigo=" + JSON.stringify(artigo), function( data ) {
-    var resp = JSON.parse(data);
-    console.log(resp, temp);
-    assert.equal('artigo atualizado com sucesso!', resp.msg);
-    done();
-  })
-    .fail(function(request, status, error) {
-      throw "artigo.create()=" + request.responseText;
-    });
-});
-
-//
-// Delete
-//
-QUnit.skip("delete", function( assert ) {
-  var done = assert.async();
-  $.post("/fulia/backend/artigos/delete.php", "id=" + temp.id, function( data ) {
-    var resp = JSON.parse(data);
-    console.log(resp, temp);
-    assert.equal('artigo deletado com sucesso!', resp.msg);
-    done();
-  })
-    .fail(function(request, status, error) {
-      throw "artigo.create()=" + request.responseText;
-    });
 });
