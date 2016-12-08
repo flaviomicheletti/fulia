1b230a7eb0cbf05aa88942e885feeaf9364ba685
Flávio Micheletti <flaviomicheletti@hotmail.com>
2016-12-08 15:23:02 -0200

    Revert "App.php"
    This reverts commit 71915c9b7697c9d61b2e715afe5ff2ff8c4743a9.

--- App.php
+++ App.php
@@ -1,10 +1,4 @@
 <?php
-ini_set('display_errors', 'on');
-ini_set('track_errors', 'on');
-ini_set('html_errors', 'on');
-ini_set('error_reporting', E_ALL);
-error_reporting(E_ALL);
-
 /**
  * Arquivo bootstrap
  */
@@ -14,7 +8,7 @@ error_reporting(E_ALL);
  *
  */
 session_start();
-date_default_timezone_set('America/Sao_Paulo');
+
 
 /**
 * Caminho relativo da raiz do sistema
